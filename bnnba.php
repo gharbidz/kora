f448e32f9c53ffd3d91e9d040d1ee42435dcd2de
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-21 21:39:59 +0100

    Create bnnba.php

---
+++ bnnba.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports nba",
+    "stream_url": "https:\/\/todtv-live-ent-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-40-dt\/DASH\/playlist_tv.mpd?hdnts=st=1742520108~exp=1742696508~acl=*\/Content\/Channel\/svc-spo-hd-40-dt\/DASH\/*~id=9DJC2AMWXMT8~data=aXA9MTU2LjE3OC4zMi40Ng~hmac=7f6a5404f1cf4e51be6d15adce19fdce7ec3c4530830207529a2f270b8910aef",
+    "drm_keys": {
+        " f64f7ec678853863bc60dc74d9834f14": "4dceab60a7c4288484ea856f8c41f56b"
+    }
+}
