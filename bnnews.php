8016ccb0b2c8da72cf725e53b67370e151ef36eb
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-21 21:41:54 +0100

    Create bnnews.php

---
+++ bnnews.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports news",
+    "stream_url": "https:\/\/todtv-live-ent-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-39-dt\/DASH\/playlist_tv.mpd?hdnts=st=1742492601~exp=1742669001~acl=*\/Content\/Channel\/svc-spo-hd-39-dt\/DASH\/*~id=9DJC2AMWXMT8~data=aXA9MTU2LjE3OC4zMi40Ng~hmac=ebcdd3f13be3f2b2352935bfecfb36ed12b7c80b145f3bdfb8a310f68ecf2737",
+    "drm_keys": {
+        "7faafe485db7395b9583222a3a6e789d": "163906ba32dd3d611aad5789cbd00858"
+    }
+}
