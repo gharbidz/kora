a7fa7aa9ca5a2f3d841f8ee9cea0df36fc8eddb9
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-21 21:38:59 +0100

    Create bnx3.php

---
+++ bnx3.php
@@ -0,0 +1,7 @@
+{
+    "title": "\tbein sports x3",
+    "stream_url": "https:\/\/todtv-live-spo-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-19-dt\/DASH\/playlist.mpd?hdnts=st=1742501103~exp=1742677503~acl=*\/Content\/Channel\/svc-spo-hd-19-dt\/DASH\/*~id=9DJC2AMWXMT8~data=aXA9MTU2LjE3OC4zMi40Ng~hmac=320772a25e1eb85802d270e5dc39992c2730ebd33d1eb07fbc7cfcfa3f0a926e",
+    "drm_keys": {
+        " 70321d024ed533a38ab945a35411a765": "ff7728dcdbe4bd28951d12f5cda9c4c6"
+    }
+}
