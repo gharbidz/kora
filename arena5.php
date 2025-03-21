2edb1f862f74e6e89ce748c1536acea49fb318f2
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-21 21:48:41 +0100

    Create arena5.php

---
+++ arena5.php
@@ -0,0 +1,9 @@
+{
+    "name": "ARENA SPORTS 5",
+    "stream_url": "https:\/\/bpcdnmanprod.nexttv.ht.hr\/bpk-tv\/arena5\/default\/index.mpd",
+    "drm_keys": {
+        "34a709bbeac052f293c778c0770829fd": "2b801fde1ad4839d4080d6ec6b0ec4ce",
+        "e473ec5d96f45a9f9a22eb665b5b1ea9": "ace54ce15b69d249b00395c9e6b85b90",
+        "18d3fba08f3152c38f14151219fa9792": "78b0c5b961351e482be81287466ecb08"
+    }
+}
