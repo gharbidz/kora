b76ae004b5352a82945e1cec1abca8f3176aaf40
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-21 21:41:15 +0100

    Create bnen2.php

---
+++ bnen2.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports en2",
+    "stream_url": "https:\/\/todtv-live-spo-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-21-dt\/DASH\/playlist.mpd?hdnts=st=1742573495~exp=1742749895~acl=*\/Content\/Channel\/svc-spo-hd-21-dt\/DASH\/*~id=9DJC2AMWXMT8~data=aXA9MTU2LjE3OS4xNzQuMjQz~hmac=dc202938f156f27c3bf57f7a4331724093715dac2b491e677b82fe9daacf95e2",
+    "drm_keys": {
+        "  b825c808bc7c3f7f95116c7d2159b35f": "0eea8f8b80ea21bf169fc3d91bec1702"
+    }
+}
