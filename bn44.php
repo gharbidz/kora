5a4ac8376cb5b9561cfc96ea833716a6b4d881c3
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-21 21:19:48 +0100

    Create bn44.php

---
+++ bn44.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports 4",
+    "stream_url": "https:\/\/todtv-live-spo-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-11-dt\/DASH\/playlist.mpd?hdnts=st=1742490976~exp=1742667376~acl=*\/Content\/Channel\/svc-spo-hd-11-dt\/DASH\/*~id=9DJC2AMWXMT8~data=aXA9MTU2LjE3OC4zMi40Ng~hmac=ed30c5cec942ac83491ffaa8c7d6e0e32b896a7c09f4aa0cd71897971bfc4005",
+    "drm_keys": {
+        "468db5e499dd3b1c9a3c5c9969e2aea8": "1a5e0247aaefad74c5912064b64859e2"
+    }
+}
