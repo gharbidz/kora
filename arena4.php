f20224241e083121d367c970c4db0e5971ed4036
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-21 21:48:16 +0100

    Create arena4.php

---
+++ arena4.php
@@ -0,0 +1,9 @@
+{
+    "name": "ARENA SPORTS 4",
+    "stream_url": "https:\/\/bpcdnmanprod.nexttv.ht.hr\/bpk-tv\/arena4\/default\/index.mpd",
+    "drm_keys": {
+        "77ce6938c781526e999b688992f27764": "bf1178841770fdba74f8629f0f06d2c0",
+        "68cd661dbc8352fda000c532840f6340": "0255fb620f4f707ea7ba090058718306",
+        "89d1dc54692e5b4fb5995fc0953bf5bf": "31b8953ab60b90fefc93d9bd1b806fbc"
+    }
+}
