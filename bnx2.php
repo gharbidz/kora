57507ba84d6e0e97699652c6f1d5d22f2de81561
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-21 21:38:31 +0100

    Create bnx2.php

---
+++ bnx2.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports x2",
+    "stream_url": "https:\/\/todtv-live-spo-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-18-dt\/DASH\/playlist.mpd?hdnts=st=1742501195~exp=1742677595~acl=*\/Content\/Channel\/svc-spo-hd-18-dt\/DASH\/*~id=9DJC2AMWXMT8~data=aXA9MTU2LjE3OC4zMi40Ng~hmac=68704a9a1400cb0d0a1288cb34e02559ee4ed88825010a63d3e3b6ec7193e53e",
+    "drm_keys": {
+        " c800955c66bc3690af22486d2d5bce75": "014ff13e55476109eeff11aaf5270f6a"
+    }
+}
