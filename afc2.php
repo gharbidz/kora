178a4cbac7582aa87a27f8120a6352a7d02dbd8c
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-21 21:51:41 +0100

    Create afc2.php

---
+++ afc2.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports afc2",
+    "stream_url": "https:\/\/live-d-02-todtv-db.akamaized.net\/variant\/v1blackout\/spo-hd-38-d-shortdvr\/DASH_DASH\/Live\/channel(spo-hd-32)\/manifest.mpd?hdnts=st=1742475594~exp=1742489994~acl=\/variant\/v1blackout\/spo-hd-38-d-shortdvr\/*~data=877ccfc4-502b-4d55-a336-d89448f13409~hmac=0d0b79fd6ca766d3ed9888f6e018a82ab4ebcfa4f60fdf2c12665531445a3708&hdcore=2.11.3",
+    "drm_keys": {
+        "0a7934dddc3136a6922584b96c3fd1e5": "676e6d1dd00bfbe266003efaf0e3aa02"
+    }
+}
