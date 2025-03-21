547dc10e72a401588c12cce6b98e856d6661314b
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-21 21:35:42 +0100

    Create bn3.php

---
+++ bn3.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports 3",
+    "stream_url": "https:\/\/todtv-live-spo-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-10-dt\/DASH\/playlist.mpd?hdnts=st=1742485022~exp=1742661422~acl=*\/Content\/Channel\/svc-spo-hd-10-dt\/DASH\/*~id=ADY4HH4AZMTJ~data=aXA9MjEzLjYuOTMuMjA1~hmac=d6bee1cad98f9e654fe2758f664f5c7531118c3495c2a1a8f3b20fbfa70caa1e",
+    "drm_keys": {
+        "cac9184d65ce37898b410b025f3ad20a": "e14cb7845b06283780efef93eb23eaf6"
+    }
+}
