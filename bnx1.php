f986f8a84426f452e1374a2247ba6b6b423fb454
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-22 20:49:52 +0100

    Update bnx1.php

--- bnx1.php
+++ bnx1.php
@@ -1,6 +1,6 @@
 {
     "title": "bein sports x1",
-    "stream_url": "https:\/\/todtv-live-spo-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-17-dt\/DASH\/playlist.mpd?hdnts=st=1742500785~exp=1742677185~acl=*\/Content\/Channel\/svc-spo-hd-17-dt\/DASH\/*~id=9DJC2AMWXMT8~data=aXA9MTU2LjE3OC4zMi40Ng~hmac=4f44144cde48c7752535d662beb3328c222bd73ebdba57f5e5ecdf2abbdcb680",
+    "stream_url": "https:\/\/todtv-live-ent-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-17-dt\/DASH\/playlist_tv.mpd?hdnts=st=1742626473~exp=1742802873~acl=*\/Content\/Channel\/svc-spo-hd-17-dt\/DASH\/*~id=9HCDZELMW3RE~data=aXA9MTkzLjEwOC4xMzAuMjI~hmac=f05631f5823d5bac2f1d144a61f69a7a457908567e0cd9f236ab76b4f35258d8",
     "drm_keys": {
         "6cc4dd3a5497310ba2e34ceeacd1c19b": "e9c5d24ea414d0a2a7100fc1fc36a387"
     }
