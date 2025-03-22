4ffb126afb339149e4415ae97199eb43697532af
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-22 20:49:24 +0100

    Update bn9.php

--- bn9.php
+++ bn9.php
@@ -1,6 +1,6 @@
 {
     "title": "bein sports 9",
-    "stream_url": "https:\/\/todtv-live-spo-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-16-dt\/DASH\/playlist.mpd?hdnts=st=1742492103~exp=1742668503~acl=*\/Content\/Channel\/svc-spo-hd-16-dt\/DASH\/*~id=9DJC2AMWXMT8~data=aXA9MTU2LjE3OC4zMi40Ng~hmac=30818c927413665465e8fa2b89490979e4b53694fba11842f09d85a63447a4cc",
+    "stream_url": "https:\/\/todtv-live-ent-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-16-dt\/DASH\/playlist_tv.mpd?hdnts=st=1742626283~exp=1742802683~acl=*\/Content\/Channel\/svc-spo-hd-16-dt\/DASH\/*~id=9HCDZELMW3RE~data=aXA9MTkzLjEwOC4xMzAuMjI~hmac=d7ae23889818fcbcbe2c46ce0272c975c93d5714767698cdbb103140453ffe18",
     "drm_keys": {
         "98eedea3e547368886168bb7f8d2e903": "305670ff7b7495b093e761a5e550697d"
     }
