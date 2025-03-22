2a2def680bfe652b10c5978f73ea42728da62d8e
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-22 20:48:03 +0100

    Update bn7.php

--- bn7.php
+++ bn7.php
@@ -1,6 +1,6 @@
 {
     "title": "bein sports 7",
-    "stream_url": "https:\/\/todtv-live-spo-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-14-dt\/DASH\/playlist.mpd?hdnts=st=1742490125~exp=1742666525~acl=*\/Content\/Channel\/svc-spo-hd-14-dt\/DASH\/*~id=9DJC2AMWXMT8~data=aXA9MTU2LjE3OC4zMi40Ng~hmac=c4fb360ceb5417efd16ae063b8623fead158b94149e3cee733593dc6e000a37b",
+    "stream_url": "https:\/\/todtv-live-ent-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-14-dt\/DASH\/playlist_tv.mpd?hdnts=st=1742626217~exp=1742802617~acl=*\/Content\/Channel\/svc-spo-hd-14-dt\/DASH\/*~id=9HCDZELMW3RE~data=aXA9MTkzLjEwOC4xMzAuMjI~hmac=7b624a25b2b2735979cbb80746f29e0caaca8641caf5a7a6b2d2975bdf245fb7",
     "drm_keys": {
         "243258212d17399f9e04423b450e45ab": "4d251bfc4fc1c305adc2c07594236253"
     }
