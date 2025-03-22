f8c14e50e406f57917dce8b20fb651927c5e4637
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-22 20:48:43 +0100

    Update bn8.php

--- bn8.php
+++ bn8.php
@@ -1,6 +1,6 @@
 {
     "title": "bein sports 8",
-    "stream_url": "https:\/\/todtv-live-spo-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-15-dt\/DASH\/playlist.mpd?hdnts=st=1742491677~exp=1742668077~acl=*\/Content\/Channel\/svc-spo-hd-15-dt\/DASH\/*~id=9DJC2AMWXMT8~data=aXA9MTU2LjE3OC4zMi40Ng~hmac=53942d4d406f83508a18084189d3ffb603085318e895349c86a4239fc1ac5314",
+    "stream_url": "https:\/\/todtv-live-ent-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-15-dt\/DASH\/playlist_tv.mpd?hdnts=st=1742626249~exp=1742802649~acl=*\/Content\/Channel\/svc-spo-hd-15-dt\/DASH\/*~id=9HCDZELMW3RE~data=aXA9MTkzLjEwOC4xMzAuMjI~hmac=174923facf9296ed74e7704cef259b0cbdfa25df2aebc6455256a53424cd1aba",
     "drm_keys": {
         "fa58e7eb3a4d36949c71ac3ef6e869d8": "b799b788cf13c7bbba95426c94879f30"
     }
