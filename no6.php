74ea294babd709ab0814c092a3ed2df872a5df9c
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-23 14:58:46 +0100

    Create no6.php

---
+++ no6.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports 6",
+    "stream_url": "https:\/\/todtv-live-ent-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-13-dt\/DASH\/playlist_tv.mpd?hdnts=st=1742626184~exp=1742802584~acl=*\/Content\/Channel\/svc-spo-hd-13-dt\/DASH\/*~id=9HCDZELMW3RE~data=aXA9MTkzLjEwOC4xMzAuMjI~hmac=c32c28254002d67cee8f53bc11211541937b49ffe6f2ed58102eebf00d571c6e",
+    "drm_keys": {
+        "44570b198707332caab56924d253ee25": "d95efbc6594ef573a269b912e33b7f72"
+    }
+}
