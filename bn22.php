51234b19f836e215b5819c0eee93ea37d9f6d0a5
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-24 19:11:56 +0100

    Update bn22.php

--- bn22.php
+++ bn22.php
@@ -1,6 +1,6 @@
 {
     "title": "bein sports 2",
-    "stream_url": "https:\/\/todtv-live-ent-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-09-dt\/DASH\/playlist_tv.mpd?hdnts=st=1742626023~exp=1742802423~acl=*\/Content\/Channel\/svc-spo-hd-09-dt\/DASH\/*~id=9HCDZELMW3RE~data=aXA9MTkzLjEwOC4xMzAuMjI~hmac=a84673ef48879023c9316e4deeaeb6771cf4ae1ad056117a92a96d194f1e8f3e",
+    "stream_url": "https://todtv-live-ent-prod.akamaized.net/Content/Channel/svc-spo-hd-09-dt/DASH/playlist_tv.mpd?hdnts=st=1742768469~exp=1742944869~acl=*/Content/Channel/svc-spo-hd-09-dt/DASH/*~id=9HCDZELMW3RE~data=aXA9MTkzLjEwOC4xMzAuMjI~hmac=1f8808beed96fbbd5f72b3f01ed97a96b45f951a7f3b28cbdddec27f959d5437",
     "drm_keys": {
         "462b64d0595b3838b4d68eb0e6c59448": "b4d30d1bfe81d4cffc52790bb6c5d979"
     }
