8b070e9c6f0de0e312b0f87ddfd5d71c31d312d3
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-26 00:19:40 +0100

    Create ADSPORTSPRM1.php

---
+++ ADSPORTSPRM1.php
@@ -0,0 +1,7 @@
+{
+    "title": "ADSPORTSPRM1",
+    "stream_url": "https:\/\/sps1.starzplayarabia.com\/out\/v1\/41ff8fde1cd84ed5b30ab8198755356b\/index.mpd",
+    "drm_keys": {
+        "b253c726c24c7c94a3ddf9b1907e2c76": "097963d6ad73c3d712a104981de0ed42"
+    }
+}
