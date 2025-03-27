a4a8f137b9c1751a828b1c38d4c479ded4a8b550
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-27 20:53:33 +0100

    Create ba1.php

---
+++ ba1.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports 1",
+    "stream_url": "https:\/\/todtv-live-ent-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-08-dt\/DASH\/playlist_tv.mpd?hdnts=st=1742885101~exp=1743104891~acl=*\/Content\/Channel\/svc-spo-hd-08-dt\/DASH\/*~id=f79d3cea-bfa7-4e4c-9974-378be1ffa41b~data=aXA9NDEuOTYuNTcuMTcw~hmac=64d0f7a1496bc9c84df06895972b344fe29ea2220f9f9b601de961b60063a758",
+    "drm_keys": {
+        "39471a4508f1387d96867b6e8ffa1e39": "ddd1fa7cd620953469679dca5bc19e54"
+    }
+}
