1fab4a76147f6c65b7574485d7d17f07bf43f0a8
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-27 20:55:08 +0100

    Create brts.php

---
+++ brts.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports 1",
+    "stream_url": "https:\/\/todtv-live-spo-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-08-dt\/DASH\/hdntl=exp=1743118158~acl=*%2fContent%2fChannel%2fsvc-spo-hd-08-dt%2fDASH%2f*~id=ba88983c-37db-4014-9ba9-0b64e2929f10~data=hdntl,aXA9NDEuMTI5LjY1LjI0MQ~hmac=5e9b1c17c26d2253fc5dffb4ed0f48d959b5bd64d0f963ed31e3fe488bb8725b\/playlist.mpd?start=1743103200000&end=1743116400000",
+    "drm_keys": {
+        "39471a4508f1387d96867b6e8ffa1e39": "ddd1fa7cd620953469679dca5bc19e54"
+    }
+}
