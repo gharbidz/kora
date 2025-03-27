aaeeb425f46278ea55985794000412389b204313
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-27 11:47:49 +0100

    Create ar1.php

---
+++ ar1.php
@@ -0,0 +1,9 @@
+{
+    "name": "ARENA SPORTS 1",
+    "stream_url": "https:\/\/bpcdnmanprod.nexttv.ht.hr\/bpk-tv\/arena1_n\/default\/index.mpd",
+    "drm_keys": {
+        "9002ec8c3dbc55c5bccdcd6871d80fd0": "7099325123bae7810db508727bb0bc7d",
+        "c43c83acda60593d8e3607efd5010d7d": "526f4a760ef52ab85056fbc10ea27584",
+        "712090aae2065c21951b170bc3002330": "a97774e4175b80205c3cf104e9574208"
+    }
+}
