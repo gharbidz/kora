df1cc5e2987caecb5e51f3e5d4353df660dc480b
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-03-27 20:37:33 +0100

    Update bnb1.php

--- bnb1.php
+++ bnb1.php
@@ -1,6 +1,6 @@
 {
     "title": "bein sports 1",
-    "stream_url": "https:\/\/todtv-live-ent-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-08-dt\/DASH\/playlist_tv.mpd?hdnts=st=1742768516~exp=1742944916~acl=*\/Content\/Channel\/svc-spo-hd-08-dt\/DASH\/*~id=9HCDZELMW3RE~data=aXA9MTkzLjEwOC4xMzAuMjI~hmac=0edc4705990769ad33a5bebe13d539b040b8bddf868eb4f49693acfcf7b27fc0",
+    "stream_url": "https:\/\/todtv-live-ent-prod.akamaized.net\/Content\/Channel\/svc-spo-hd-08-dt\/DASH\/playlist_tv.mpd?hdnts=st=1742885101~exp=1743061501~acl=*\/Content\/Channel\/svc-spo-hd-08-dt\/DASH\/*~id=A5PBQFDFH2JD~data=aXA9NDYuMjAyLjE3Mi42Nw~hmac=362cfcd9668dbafd9bdf6ce0b42345352ab2f46cf397775cd7f9d49c6fcce5a5",
     "drm_keys": {
         "39471a4508f1387d96867b6e8ffa1e39": "ddd1fa7cd620953469679dca5bc19e54"
     }
