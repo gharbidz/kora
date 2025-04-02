2cb15e7d25644eebfb8289a0249b21a8d2f98788
gharbidz <167705938+gharbidz@users.noreply.github.com>
2025-04-02 13:45:16 +0100

    Create babra.php

---
+++ babra.php
@@ -0,0 +1,7 @@
+{
+    "title": "bein sports 6",
+    "stream_url": "https://todtv-live-spo-prod.akamaized.net/Content/Channel/svc-spo-hd-13-dt/DASH/hdntl=exp=1743611999~acl=*%2fContent%2fChannel%2fsvc-spo-hd-13-dt%2fDASH%2f*~id=a720ebec-0d88-4916-ba6f-b38b89156128~data=hdntl,aXA9NDEuMTExLjIzNC45OA~hmac=80ddaa4f2de7dec45eff433d9afad0113c2e79a000e95fd1e273f973ee60ab7a/playlist.mpd?start=1743595500000&end=1743609600000",
+    "drm_keys": {
+        "44570b198707332caab56924d253ee25": "d95efbc6594ef573a269b912e33b7f72"
+    }
+}
